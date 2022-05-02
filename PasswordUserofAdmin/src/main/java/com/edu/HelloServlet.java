34b3876fca779612afb883d3dac94f0bdc09e38d
Acer <Acer@Lishanth>
2022-05-02 23:57:45 +0530

    first commit

---
+++ PasswordUserofAdmin/src/main/java/com/edu/HelloServlet.java
@@ -0,0 +1,47 @@
+package com.edu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out=response.getWriter();
+		String un=request.getParameter("uname");
+		String up=request.getParameter("upass");
+		if(un.equalsIgnoreCase("admin")&& up.equals("admin123"))
+		{
+			out.println("User is valid");
+		}
+		else
+		{
+			out.println("User is invalid");
+		}
+		
+	}
+
+}
